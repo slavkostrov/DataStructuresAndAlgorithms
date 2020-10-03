b5a9b084f3f1c535635d2d3ad15571470c23001c
Slava <slavkostrov@gmail.com>
2020-10-03 23:37:42 +0300

    add

--- Trees/tree.h
+++ Trees/tree.h
@@ -21,6 +21,40 @@ public:
 		this->size = 0;
 		this->Root = nullptr;
 	}
+
+	int16_t push(T info){
+		if (this->node_ == nullptr)
+		{
+			this->node_ = new node(info);
+			return 1;
+		}
+		if (info > this->Root->info)
+			if (this->Root->right_child == nullptr)
+				this->Root->right_child = new node(info);
+			else
+				return push(info, this->Root->right_child);
+		else
+			if (this->Root->left_child == nullptr)
+				this->Root->left_child = new node(info)
+			else
+				return push(info, this->Root->left_child);
+		return -1;
+	}
+
+	int16_t push(T info, node* node_){
+		if (info > node_->info)
+			if (node_->right_child == nullptr)
+				node_->right_child = new node(info);
+			else
+				return push(info, node_->right_child);
+		else
+			if (node_->left_child == nullptr)
+				node_->left_child = new node(info)
+			else
+				return push(info, node_->left_child);
+		return -1;
+	}
+
 }
 
 #endif
