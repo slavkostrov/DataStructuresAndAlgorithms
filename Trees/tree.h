354dd35214b281337f32c072855a4ef53179c714
Slava <slavkostrov@gmail.com>
2020-10-03 23:43:21 +0300

    fix

--- Trees/tree.h
+++ Trees/tree.h
@@ -22,37 +22,16 @@ public:
 		this->Root = nullptr;
 	}
 
-	int16_t push(T info){
-		if (this->node_ == nullptr)
+	int16_t push(T info, node** node_ = &(this->Root)){
+		if (*(node_) = nullptr)
 		{
-			this->node_ = new node(info);
+			*(node_) = new node(info);
 			return 1;
 		}
-		if (info > this->Root->info)
-			if (this->Root->right_child == nullptr)
-				this->Root->right_child = new node(info);
-			else
-				return push(info, this->Root->right_child);
+		if (info > *(node_)->info)
+			return this->push(info, &(*(node_)->right_child));
 		else
-			if (this->Root->left_child == nullptr)
-				this->Root->left_child = new node(info)
-			else
-				return push(info, this->Root->left_child);
-		return -1;
-	}
-
-	int16_t push(T info, node* node_){
-		if (info > node_->info)
-			if (node_->right_child == nullptr)
-				node_->right_child = new node(info);
-			else
-				return push(info, node_->right_child);
-		else
-			if (node_->left_child == nullptr)
-				node_->left_child = new node(info)
-			else
-				return push(info, node_->left_child);
-		return -1;
+			return this->push(info, &(*(node_)>left_child));
 	}
 
 }
