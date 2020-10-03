6a03d143114a0a61ff5ff6d5710f9afa26cbad49
Slava <slavkostrov@gmail.com>
2020-10-03 23:51:03 +0300

    search

--- Trees/tree.h
+++ Trees/tree.h
@@ -1,11 +1,12 @@
 #ifndef TREE_H
 #define TREE_H
 
-template <class T>
+template <typename T>
 class BinaryTree{
 private:
 	node* Root;
 	int32_t size;
+	bool empty;
 public:
 	struct node{
 		node(T info){
@@ -20,9 +21,11 @@ public:
 	BinaryTree(){
 		this->size = 0;
 		this->Root = nullptr;
+		this->empty = true;
 	}
 
 	int16_t push(T info, node** node_ = &(this->Root)){
+		this->size += 1;
 		if (*(node_) = nullptr)
 		{
 			*(node_) = new node(info);
@@ -34,6 +37,17 @@ public:
 			return this->push(info, &(*(node_)>left_child));
 	}
 
+	T* search(T value, node* node_ = this->Root){
+		if (node_ == nullptr)return nullptr;
+		if (node_->info == value)
+			return &(node_->info);
+		if (node_->info > value)
+			return search(value, node_->right_child);
+		else
+			return search(value, bode_->left_child);
+	}
+
+	
 }
 
 #endif
