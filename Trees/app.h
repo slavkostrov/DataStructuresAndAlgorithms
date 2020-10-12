0626dd49327a147e62b14367c52d983a7ca8ed92
Slava <slavkostrov@gmail.com>
2020-10-12 22:20:05 +0300

    app

---
+++ Trees/app.h
@@ -0,0 +1,9 @@
+#ifndef APP_H
+#define APP_H
+#include "names_tree.h"
+#include <iostream>
+#include <string>
+
+int32_t exec_app();
+
+#endif // !APP_H
--- Trees/names_tree_test.cpp
+++ Trees/names_tree_test.cpp
@@ -1,34 +1,6 @@
-#include <iostream>
-#include <string>
-
-#include "names_tree.h"
+#include "app.h"
 
 int main() 
 {
-	std::cout << "Enter first name: ";
-	std::string name;
-	getline(std::cin, name);
-	node* Tree = new node(name, true);
-	std::cout << "Now input next names (\"end\" = exit)\n";
-	getline(std::cin, name);
-	while (name != "end") {
-		push(&Tree, name);
-		getline(std::cin, name);
-	}
-	std::cout << '\n';
-
-	std::cout << "Print function realisation with names:\n";
-	print(Tree, false);
-	std::cout << '\n';
-	std::cout << "Print function realisation with colors:\n";
-	print(Tree);
-	std::cout << '\n';
-	std::cout << "Backward pass realisation:\n";
-	backward_pass(Tree);
-	std::cout << "\n\nSymmetric pass realisation:\n";
-	symmetric_pass(Tree);
-	std::cout << "\n\nPath length realisation: ";
-	path_length(Tree, "7");
-	std::cout << "\n\nTree depth is " << depth(Tree) << "\n";
-	return 0;
+	return exec_app();
 }
